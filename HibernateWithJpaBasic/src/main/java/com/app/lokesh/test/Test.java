400c79f676b5bfcc2cad93780e509cabd3b53e14
lokesh sukhwal <techlokesh@yahoo.com>
2023-07-11 15:08:43 +0530

    Hibernate with JPA;

---
+++ HibernateWithJpaBasic/src/main/java/com/app/lokesh/test/Test.java
@@ -0,0 +1,48 @@
+package com.app.lokesh.test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.app.lokesh.Student;
+
+public class Test {
+	public static void main(String[] args) {
+		EntityTransaction tx = null;
+		try {
+			// 1. (Loads Driver, Creates DB Connections, Prepare stmt)
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("AppDB");
+			System.out.println(emf.getClass().getName());
+
+			// 2. (TODO Operations--insert, update,....)
+			EntityManager em = emf.createEntityManager();
+			System.out.println(em.getClass().getName());
+
+			// 3. create Transaction
+			tx = em.getTransaction();
+			System.out.println(tx.getClass().getName());
+			// 4. Start Transaction
+			tx.begin();
+
+			// 5. Perform operation
+			// save data into DB
+			Student student = new Student();
+			student.setStudentId(10);
+			student.setStudentName("LOKESH");
+			student.setFee(5000.0);
+
+			em.persist(student); // SQL: INSERT INTO ...
+
+			// 6. commit
+			tx.commit();
+
+			// 7. close
+			emf.close();
+		} catch (Exception e) {
+			// rollback if any problem
+			tx.rollback();
+			e.printStackTrace();
+		}
+	}
+}
